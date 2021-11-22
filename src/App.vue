95a33e667eebcbaba15b5151850a95e3cded37d9
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-22 21:29:29 +0100

    final project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero />
+    
+    <Img />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from '@/components/hero.vue'
+import Img from '@/components/jumbotron.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    Hero,
+    Img,
   }
 }
 </script>
